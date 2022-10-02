8b5012ffb2a6fb8f1aace7c1290fb002586e8800
Sinthu010 <111531664+Sinthu010@users.noreply.github.com>
2022-10-02 20:24:16 +0100

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,38 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+// write your code here
+
+let requestedCity = prompt("Enter a city: ");
+
+if (requestedCity in weather) {
+  let celciusTemperature = Math.round(weather[requestedCity].temp);
+  let farenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  alert(
+    `It is currently ${celciusTemperature}°C (${farenheitTemperature}°F) in Paris with a humidity of ${weather[requestedCity].humidity}%`
+  );
+} else {
+  alert(
+    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${requestedCity}`
+  );
+}
